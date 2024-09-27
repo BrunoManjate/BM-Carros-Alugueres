03666bffac078ed837bcd602ff4c39d63612f6d9
Bruno F. Manjate <87826515+BrunoManjate@users.noreply.github.com>
2024-09-27 21:11:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("reserveBtn").addEventListener("click", function () {
+    alert("Você clicou em 'Reserve Agora'!");
+    // Aqui você pode redirecionar para a página de reservas ou abrir um modal
+    // window.location.href = "#reservas"; // Exemplo de redirecionamento
+});
+
